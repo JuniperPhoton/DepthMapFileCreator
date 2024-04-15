dc94653530058fdb8a036c82924a316e3433ebb5
juniperphoton <dengweichao@hotmail.com>
2024-04-15 15:40:43 +0800

    Initial Commit

---
+++ DepthMapFileCreator/DepthMapFileCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepthMapFileCreatorApp.swift
+//  DepthMapFileCreator
+//
+//  Created by Photon Juniper on 2024/4/15.
+//
+
+import SwiftUI
+
+@main
+struct DepthMapFileCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
